d4e96f0a6ce68761885e2ce2cb80009270d8b861
Amrish <amrish.m004@gmail.com>
2024-08-14 23:19:26 +0530

    Two Sum

--- ArrayProblems/Leetcode1/TwoSums.java
+++ ArrayProblems/Leetcode1/TwoSums.java
@@ -1,4 +1,23 @@
 package ArrayProblems.Leetcode1;
 
+import java.util.HashMap;
+
 public class TwoSums {
+    public int[] twoSum(int[] nums, int target) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        int sum =0;
+
+        for(int i=0; i<nums.length; i++){
+            sum = target - nums[i];
+
+            if(map.containsKey(sum)){
+                return new int[]{map.get(sum),i};
+            }
+            map.put(nums[i],i);
+        }
+
+        return new int[]{};// there is no element
+    }
 }
