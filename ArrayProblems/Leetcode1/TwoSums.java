50d11e8f5ab9438c8da2cf6f55056c968c18240e
Amrish <amrish.m004@gmail.com>
2024-08-14 23:23:25 +0530

    Two Sum

--- ArrayProblems/Leetcode1/TwoSums.java
+++ ArrayProblems/Leetcode1/TwoSums.java
@@ -8,10 +8,8 @@ public class TwoSums {
         HashMap<Integer, Integer> map = new HashMap<>();
 
         int sum =0;
-
         for(int i=0; i<nums.length; i++){
             sum = target - nums[i];
-
             if(map.containsKey(sum)){
                 return new int[]{map.get(sum),i};
             }
