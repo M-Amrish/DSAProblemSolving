8fa2f74d106dfe1d6b390fe3fb33072e6b49656a
Amrish <amrish.m004@gmail.com>
2024-08-19 11:23:31 +0530

    pivotIndex

---
+++ ArrayProblems/Leetcode_724/FindPivotIndex.java
@@ -0,0 +1,35 @@
+package ArrayProblems.Leetcode_724;
+
+class FindPivotIndex {
+    public int pivotIndex(int[] nums) {
+
+        int n = nums.length;
+        int total = 0;
+        int left = 0;
+
+        // finding total sum
+        for(int i=0; i<n; i++){
+            total += nums[i];
+        }
+
+        int right = total;
+
+        // iterating
+        for(int i=0; i<n; i++){
+
+            right -= nums[i];
+
+            // checking condition
+            if(left == right){
+                return i;
+            }
+
+            left += nums[i];
+
+        }
+
+        return -1;
+
+
+    }
+}
