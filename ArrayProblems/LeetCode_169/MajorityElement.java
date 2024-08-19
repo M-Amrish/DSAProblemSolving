fb131b5459c49346a2e1ccc9096f0e6144590239
Amrish <amrish.m004@gmail.com>
2024-08-19 11:38:39 +0530

    adding majority element

---
+++ ArrayProblems/LeetCode_169/MajorityElement.java
@@ -0,0 +1,25 @@
+package ArrayProblems.LeetCode_169;
+
+public class MajorityElement  {
+    public int majorityElement(int[] nums) {
+
+        int count = 0;
+        int element = 0;
+
+        for(int ele : nums){
+
+            if(count == 0){
+                element = ele;
+            }
+
+            if(element == ele){
+                count++;
+            }else{
+                count--;
+            }
+        }
+
+        return element;
+
+    }
+}
