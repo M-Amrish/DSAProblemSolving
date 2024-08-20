4fdc77d19a3d91a26b2ba317805c2d13fa956729
Amrish <amrish.m004@gmail.com>
2024-08-21 00:18:16 +0530

    Adding stock buying and selling.

---
+++ ArrayProblems/LeetCode_121/MaxProfit.java
@@ -0,0 +1,20 @@
+package ArrayProblems.LeetCode_121;
+
+public class MaxProfit {
+    public int maxProfit(int[] prices) {
+
+        int n = prices.length;
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0; i<n; i++){
+            min = Math.min(min, prices[i]);
+
+            max = Math.max(max, prices[i] - min);
+
+        }
+
+        return max;
+    }
+}
