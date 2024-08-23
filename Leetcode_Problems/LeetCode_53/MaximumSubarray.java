da0eaa73113ea9824558879fb8cd5e65b2b71ea6
Amrish <amrish.m004@gmail.com>
2024-08-23 21:56:37 +0530

    adding maxSubarray/Kadane's algo

---
+++ Leetcode_Problems/LeetCode_53/MaximumSubarray.java
@@ -0,0 +1,23 @@
+package Leetcode_Problems.LeetCode_53;
+
+public class MaximumSubarray {
+    public int maxSubArray(int[] nums) {
+
+        int n = nums.length;
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+
+        for(int i=0; i<n; i++){
+            sum += nums[i];
+
+            max = Math.max(max,sum);
+
+            if(sum < 0){
+                sum = 0;
+            }
+
+        }
+
+        return max;
+    }
+}
