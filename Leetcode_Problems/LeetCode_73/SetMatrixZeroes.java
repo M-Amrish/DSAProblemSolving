f8b9f8b2627d7d00d360e4a6c097cbcad2fcc082
Amrish <amrish.m004@gmail.com>
2024-08-24 13:44:52 +0530

    adding set Zeroes to Matrixs

---
+++ Leetcode_Problems/LeetCode_73/SetMatrixZeroes.java
@@ -0,0 +1,53 @@
+package Leetcode_Problems.LeetCode_73;
+
+public class SetMatrixZeroes {
+    public void setZeroes(int[][] matrix) {
+
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        boolean isRow0 = false;
+        boolean isCol0 = false;
+
+        for(int i=0; i<n; i++){
+            if(matrix[i][0] == 0){
+                isRow0 = true;
+            }
+        }
+
+        for(int j=0; j<m;j++){
+            if(matrix[0][j] == 0){
+                isCol0 = true;
+            }
+        }
+
+        for(int i=1; i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][j] == 0){
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        for(int i=1;i<n; i++){
+            for(int j=1; j<m; j++){
+                if(matrix[i][0] == 0 || matrix[0][j] ==0){
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+
+        if(isRow0){
+            for(int i=0;i<n;i++){
+                matrix[i][0]=0;
+            }
+        }
+
+        if(isCol0){
+            for(int j=0;j<m; j++){
+                matrix[0][j] = 0;
+            }
+        }
+    }
+}
