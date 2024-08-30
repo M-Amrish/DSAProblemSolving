6b5a8f6592ff212a408466f65db39060403844ad
Amrish <amrish.m004@gmail.com>
2024-08-30 22:08:43 +0530

    adding MiddleOfLInkedList

---
+++ Leetcode_Problems/LeetCode_876/MiddleOfLInkedList.java
@@ -0,0 +1,27 @@
+package Leetcode_Problems.LeetCode_876;
+
+import java.util.List;
+
+class ListNode{
+    int val;
+    ListNode next;
+
+    ListNode(int x){
+        val = x;
+    }
+}
+public class MiddleOfLInkedList {
+    public ListNode middleNode(ListNode head) {
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+
+    }
+}
