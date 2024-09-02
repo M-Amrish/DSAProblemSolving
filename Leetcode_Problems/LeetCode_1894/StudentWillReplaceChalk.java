83468cb0c140f5ffc0795cabee3414f84c58ca67
Amrish <amrish.m004@gmail.com>
2024-09-02 23:38:43 +0530

    adding StudentWillReplaceChalk

---
+++ Leetcode_Problems/LeetCode_1894/StudentWillReplaceChalk.java
@@ -0,0 +1,16 @@
+package Leetcode_Problems.LeetCode_1894;
+
+public class StudentWillReplaceChalk {
+    public int chalkReplacer(int[] chalk, int k) {
+        long accSum = 0;
+        for (int c : chalk) accSum += c;
+
+        k %= accSum;
+
+        for (int i = 0; i < chalk.length; i++) {
+            if (chalk[i] > k) return i;
+            k -= chalk[i];
+        }
+        return -1;
+    }
+}
