292fe09d5878b5b839b2bcfd5ad1e2557c551fef
Amrish <amrish.m004@gmail.com>
2024-09-15 23:30:24 +0530

    right view

---
+++ Leetcode_Problems/LeetCode_199/BinaryTreeRightSideView.java
@@ -0,0 +1,57 @@
+package Leetcode_Problems.LeetCode_199;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class TreeNode {
+     int val;
+     TreeNode left;
+     TreeNode right;
+     TreeNode() {}
+     TreeNode(int val) { this.val = val; }
+     TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+     }
+ }
+
+public class BinaryTreeRightSideView {
+
+    public List<Integer> rightSideView(TreeNode root) {
+
+        List<Integer> al = new ArrayList<>();
+
+        if(root == null) return al;
+
+        Queue<TreeNode>  q = new LinkedList<>();
+
+        q.add(root);
+
+        while( q.size() !=  0){
+
+            int size = q.size();
+
+            for(int i=1; i<=size; i++){
+
+                TreeNode rem = q.remove();
+
+                if(i == size){
+                    al.add(rem.val);
+                }
+
+                if(rem.left != null){
+                    q.add(rem.left);
+                }
+
+                if(rem.right != null){
+                    q.add(rem.right);
+                }
+
+            }
+        }
+
+        return al;
+
+    }
+}
